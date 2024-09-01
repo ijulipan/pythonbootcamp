cd86fbaa56689e272ce336036beda63f53f16b2c
Izzul Irfan <irfanizzul35@gmail.com>
2024-09-01 20:32:14 +0800

    completed nato project

---
+++ project_24/dictionary_comprehension/main.py
@@ -0,0 +1,23 @@
+## Dictionary comprehension
+# FORMAT: new_dict = {new_key:new_value for (key, value) in items if test}
+import random
+import pandas
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
